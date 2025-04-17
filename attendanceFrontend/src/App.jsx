425254d0b88594a8c47798e4b31516087ad9030e
Bhagyesh Anil Chaudhari <121716621+Bhagyesh-Chaudhari@users.noreply.github.com>
2025-04-17 23:27:11 +0530

    Teacher Side Completed

--- attendanceFrontend/src/App.jsx
+++ attendanceFrontend/src/App.jsx
@@ -1,33 +1,38 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import LoginPage from './pages/login';
-import TeacherDashBoard from "./pages/TeacherDashBoard";
-import AttendanceForm from './pages/AttendanceForm';
-import ChangePasswordPage from './pages/ChangePasswordPage';
-import AdminDashboard from './pages/AdminDashboard';
-import AddClasses from './pages/AddClasses';
-import AddSubjects from './pages/AddSubjects';
-import AddBranchYearSubject from './pages/AddBranchYearSubject';
-import AddTeacher from './pages/AddTeacher';
-import AssignTeacher from './pages/AssignTeacher';
-import AttendancePage from './pages/AttendancePage';
+import TeacherDashBoard from "./pages/Teacher/TeacherDashBoard";
+import AttendanceForm from './pages/Teacher/AttendanceForm';
+import AttendancePage from './pages/Teacher/AttendancePage';
+import ChangePasswordPage from './pages/Teacher/ChangePasswordPage';
+
+import AdminDashboard from './pages/Admin/AdminDashboard';
+import AddClasses from './pages/Admin/AddClasses';
+import AddSubjects from './pages/Admin/AddSubjects';
+import AddBranchYearSubject from './pages/Admin/AddBranchYearSubject';
+import AddTeacher from './pages/Admin/AddTeacher';
+import AssignTeacher from './pages/Admin/AssignTeacher';
 
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<LoginPage />} />
+        {/* Teacher Routes */}
         <Route path="/teacher-dashboard" element={<TeacherDashBoard />} />
         <Route path="/attendanceForm" element={<AttendanceForm />} />
         <Route path="/change-password" element={<ChangePasswordPage />} />
+        <Route path="/attendance-page" element={<AttendancePage />} />
+        {/* Admin Routes */}
         <Route path="/admin-dashboard" element={<AdminDashboard/>} />
         <Route path="/create-class" element={<AddClasses />} />
         <Route path="/create-subject" element={<AddSubjects />} />
         <Route path="/add-subject-branch" element={<AddBranchYearSubject />} /> 
         <Route path="/create-teacher" element={<AddTeacher />} /> 
         <Route path="/assign-teacher" element={<AssignTeacher />} />
-        <Route path="/attendance-page" element={<AttendancePage />} />
       </Routes>
     </Router>
   );
